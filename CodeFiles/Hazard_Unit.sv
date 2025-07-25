2b2cf05bbe2efa3d5c1cb9f079957917cdd8bdd6
avyakth devarajan <143103757+avyakthd@users.noreply.github.com>
2025-07-25 12:53:17 +0530

    Update Hazard_Unit.sv

--- CodeFiles/Hazard_Unit.sv
+++ CodeFiles/Hazard_Unit.sv
@@ -7,16 +7,20 @@ module Hazard_Unit (
   input reg_idx_t rd,
   // from IF_ID
   input reg_idx_t rs1, rs2,
-
+  // from Top.sv
+  input uses_rs1, uses_rs2,
   // direct wired-connection
   output logic Stall
 );
 
   always_comb begin
     Stall = 0;
-    if (MReg == from_DataMem)
-      if (rd != 0 && ((rd == rs1) || (rd == rs2)))
+    if (MReg == from_DataMem && rd != 0) begin
+      if (uses_rs1 && (rd == rs1))
         Stall = 1;
-  end 
-
+      else if (uses_rs2 && (rd == rs2))
+        Stall = 1;
+    end
+  end
+  
 endmodule
